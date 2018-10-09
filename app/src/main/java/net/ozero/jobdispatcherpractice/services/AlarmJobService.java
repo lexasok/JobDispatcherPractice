744dacd5382682f91099c4df317e5fe1f73dc7c6
alex <sokolov@ozero.net>
2018-10-10 02:35:59 +0300

    Alarm notification implemented

--- app/src/main/java/net/ozero/jobdispatcherpractice/services/AlarmJobService.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/services/AlarmJobService.java
@@ -1,17 +1,39 @@
 package net.ozero.jobdispatcherpractice.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+
+import net.ozero.jobdispatcherpractice.R;
 
 public class AlarmJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters job) {
-        return false;
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Title")
+                        .setContentText("Notification text");
+
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        assert notificationManager != null;
+        notificationManager.notify(1, notification);
+
+        return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters job) {
         return false;
+
+
     }
 }
