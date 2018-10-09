02b466a64760a1b6b4b9b6bfa9b935c2b79b38bf
alex <sokolov@ozero.net>
2018-10-10 02:44:29 +0300

    Logs added

--- app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
@@ -2,6 +2,7 @@ package net.ozero.jobdispatcherpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
@@ -24,12 +25,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(getClass().getName(), "onCreate");
     }
 
 
     public void setAlarmButtonClicked(View view) {
 
         setJob();
+
+        Log.i(getClass().getName(), "job set");
     }
 
     private void setJob() {
--- app/src/main/java/net/ozero/jobdispatcherpractice/services/AlarmJobService.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/services/AlarmJobService.java
@@ -3,6 +3,7 @@ package net.ozero.jobdispatcherpractice.services;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
@@ -13,6 +14,8 @@ public class AlarmJobService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters job) {
+
+        Log.i(getClass().getName(), "onStartJob");
 
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(this)
@@ -27,11 +30,15 @@ public class AlarmJobService extends JobService {
         assert notificationManager != null;
         notificationManager.notify(1, notification);
 
+        jobFinished(job, true);
+
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters job) {
+
+        Log.i(getClass().getName(), "onStopJob");
         return false;
 
 
