5297729fb5737a564db45723ddc6d3c97a6a3573
alex <sokolov@ozero.net>
2018-10-10 02:06:57 +0300

    Lib added

--- app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
@@ -2,6 +2,10 @@ package net.ozero.jobdispatcherpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.xmlpull.v1.sax2.Driver;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void setAlarmButtonClicked(View view) {
+
+    }
+
+    private void setJob() {
+
+
+    }
 }
