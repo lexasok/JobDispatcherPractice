180a15579d720bf1324a4a6002d5c05a2ab09b4a
alex <sokolov@ozero.net>
2018-10-10 18:49:31 +0300

    Timeout changed to look, will the job done after reboot

--- app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/MainActivity.java
@@ -8,19 +8,16 @@ import android.view.View;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
-import com.firebase.jobdispatcher.JobTrigger;
 import com.firebase.jobdispatcher.Lifetime;
 import com.firebase.jobdispatcher.Trigger;
 
 import net.ozero.jobdispatcherpractice.services.AlarmJobService;
 
-import org.xmlpull.v1.sax2.Driver;
-
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String JOB_TAG = "alarm_job";
-    public static final int TIMEOUT_IN_SECONDS = 10;
+    public static final int TIMEOUT_IN_SECONDS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 .setService(AlarmJobService.class)
                 .setTag(JOB_TAG)
                 .setLifetime(Lifetime.FOREVER)
-                .setTrigger(Trigger.executionWindow(TIMEOUT_IN_SECONDS, TIMEOUT_IN_SECONDS))
+                .setTrigger(Trigger.executionWindow(TIMEOUT_IN_SECONDS, TIMEOUT_IN_SECONDS + 10))
                 .build();
 
         firebaseJobDispatcher.mustSchedule(job);
