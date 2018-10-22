31af2932927f60391cfc27577e368ee895840141
alex <sokolov@ozero.net>
2018-10-22 11:46:44 +0300

    Methods added

--- app/src/main/java/net/ozero/jobdispatcherpractice/services/activities/MainActivity.java
+++ app/src/main/java/net/ozero/jobdispatcherpractice/services/activities/MainActivity.java
@@ -1,10 +1,14 @@
 package net.ozero.jobdispatcherpractice.services.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
@@ -19,35 +23,55 @@ import net.ozero.jobdispatcherpractice.services.AlarmService;
 
 public class MainActivity extends AppCompatActivity {
 
+    //preferences
+    private static final String PREFERENCES_SESSION = "session";
+    private static final String KEY_ID = "id";
+
     public static final String JOB_TAG = "alarm_job";
     public static final int TIMEOUT_IN_SECONDS = 5;
     public static final String EXTRA_TIME = "seconds";
+
+    private EditText mDelayInput;
+    private Button mSetAlarmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Log.i(getClass().getName(), "onCreate");
+        initViews();
+    }
+
+    private void initViews() {
+        mDelayInput = findViewById(R.id.delayInput);
+        mSetAlarmButton = findViewById(R.id.buttonSetAlarm);
+        String title = getResources().getString(R.string.title_button_set_alarm)+ " " + nextId();
+        mSetAlarmButton.setText(title);
+    }
+
+    private int nextId() {
+        SharedPreferences preferences = getSharedPreferences(PREFERENCES_SESSION, MODE_PRIVATE);
+        int id = preferences.getInt(KEY_ID, 1);
+        preferences.edit().putInt(KEY_ID, id + 1).apply();
+        return id;
     }
 
     public void setAlarmButtonClicked(View view) {
-        int seconds = 0;
+        int seconds = Integer.parseInt(mDelayInput.getText().toString());
+        int id = nextId() - 1;
+        String title = getResources().getString(R.string.title_button_set_alarm) + " " + (id + 1);
+        mSetAlarmButton.setText(title);
 
-        switch (view.getId()) {
-            case R.id.buttonSet10secAlarm:
-                seconds = 10;
-                break;
-            case R.id.buttonSet1MinAlarm:
-                seconds = 60;
-                break;
-        }
-        setJobInService(seconds);
+        setJobInService(id, seconds);
     }
 
-    private  void  setJobInService(int seconds) {
+    private  void  setJobInService(int id, int seconds) {
         Intent intent = new Intent(this, AlarmService.class);
         intent.putExtra(EXTRA_TIME, seconds);
         startService(intent);
+
+        Toast.makeText(this, "Alarm set, id: " + id, Toast.LENGTH_LONG).show();
     }
 
     private void setJobHere(int seconds) {
